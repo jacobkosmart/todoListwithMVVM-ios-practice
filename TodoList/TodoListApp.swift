9576c15456e8f29c01e19470937371fc10c01e95
jacobkosmart <jacobkosmart@gmail.com>
2022-01-14 13:48:07 +0900

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Jacob Ko on 2022/01/14.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
